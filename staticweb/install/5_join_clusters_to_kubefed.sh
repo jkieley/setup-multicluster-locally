be72f7d9dfeee66d60ed749b16ced116d444417a
jkieley <PNG4XjJNPuBb9H>
2019-09-01 21:57:25 -0700

    update join clusters script

--- staticweb/install/5_join_clusters_to_kubefed.sh
+++ staticweb/install/5_join_clusters_to_kubefed.sh
@@ -4,7 +4,9 @@
 
 kubefedctl join cluster1 \
     --cluster-context cluster1 \
-    --host-cluster-context cluster1 --v=2
+    --host-cluster-context cluster2 --v=2
 kubefedctl join cluster2 \
     --cluster-context cluster2 \
-    --host-cluster-context cluster1 --v=2
+    --host-cluster-context cluster2 --v=2
+
+# see check_status_of_joined_kubefed_clusters.sh
